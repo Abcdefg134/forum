8ae5e7c6be583c02ca0520558bba5fc10d6f50b6
abcdefg134 <toan198vn1@gmail.com>
2021-07-02 15:02:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv')
 const http = require('http').Server(app)
 const io = require('socket.io')(http, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://optimistic-brattain-ecdb7c.netlify.app/",
         credentials: true
     }
 })
